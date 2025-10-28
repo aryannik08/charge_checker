c9691eff88da3d15919e65ffc4dd520b54e227ed
aryannik08 <aryannik08@gmail.com>
2025-10-28 15:05:02 +0330

    test push

--- lib/presentation/ui/base/splash_page/splash_page.dart
+++ lib/presentation/ui/base/splash_page/splash_page.dart
@@ -7,7 +7,7 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ThemeData theme = Theme.of(context);
+
 
     return GetBuilder(
       init: SplashController(),
